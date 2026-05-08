644d6054668e86c6f71d4d70f713227206c47d9d
tsugimot <sugimototakuto@sugimototakutononotobukkukonpyuta.local>
2026-05-08 13:03:24 +0900

    submit

--- main/scan_integers.c
+++ main/scan_integers.c
@@ -1,12 +1,37 @@
 #include "push_swap.h"
 
+int	ft_count_words(char *str)
+{
+	int	number;
+
+	number = 0;
+	while (*str++)
+	{
+		if (*str == ' ')
+			str++;
+		if (*str)
+			number++;
+		while (*str != ' ')
+			str++;
+	}
+	return (number);
+}
+
 int	*ft_scan_integers (char *str)
 {
-	int	*integers;
+	char	*c_str;
+	int		*integers;
 
+	c_str = (char *)malloc(sizeof(char) * ft_count_words (str));
+	if (!c_str)
+		return (NULL);
 	integers = (int *)malloc(sizeof(int) * ft_strlen (str));
-	while (*str)
+	if (!integers)
 	{
-		if ()
+		free (c_str);
+		return (NULL);
 	}
+	while (*c_str)
+		*integers++ = *c_str++ - '0';
+	return (integers);
 }
